f20fe7ab8c01a0b475b902bb0949d44fa58d53cd
sych74 <sych74@gmail.com>
2016-09-14 14:48:33 +0200

    Update build-cluster.js

--- build-cluster.js
+++ build-cluster.js
@@ -14,11 +14,10 @@ for (var i = 0; i < nodes.length; i++) {
 resp = [];
 for (var i = 0; i < nodes.length; i++) { 
     if (nodes[i].nodeGroup != nodeGroup) continue; 
-    resp.push(jelastic.env.control.SetDockerEnvVars(envName, session, nodes[i].id, '{"SERVERS":"'+IPs.join(' ')+'"}'));
+    resp.push(jelastic.env.control.SetDockerEnvVars(envName, session, nodes[i].id, '{SERVERS:"'+IPs.join(' ')+'"}'));
 }
 
 return {
     result: 0,
-    response: resp,
-    data: '{"SERVERS":"'+IPs.join(' ')+'"}'
+    response: resp
 }
