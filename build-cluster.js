434e90c7b0ca34eb4a1b5a79464a5fe9155fa2cf
sych74 <sych74@gmail.com>
2016-09-15 10:25:22 +0200

    Update build-cluster.js

--- build-cluster.js
+++ build-cluster.js
@@ -1,5 +1,5 @@
 //@auth
-//@req(nodeGroup, path)
+//@req(nodeGroup, volumePath)
 
 var envName = '${env.envName}', 
 nodes = jelastic.env.control.GetEnvInfo(envName, session).nodes,
@@ -7,15 +7,17 @@ IPs = [], resp = [];
 
 for (var i = 0; i < nodes.length; i++) { 
   if (nodes[i].nodeGroup != nodeGroup) continue; 
-  IPs.push(nodes[i].address + ':' + path);
+  IPs.push(nodes[i].address + ':' + volumePath);
 }
 
-if (IPs.length > 1) { 
-  for (var i = 0; i < nodes.length; i++) { 
+//if there is only one server then we should specify only volumePath w/o IP
+if (IPs.length == 1) IPs[0] = volumePath;
+  
+for (var i = 0; i < nodes.length; i++) { 
       if (nodes[i].nodeGroup != nodeGroup) continue; 
       resp.push(jelastic.env.control.SetDockerRunCmd(envName, session, nodes[i].id, IPs.join(' ') + " --address :$PORT"));
-  }
 }
+
 
 return {
     result: 0,
