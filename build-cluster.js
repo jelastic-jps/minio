1cb9eb50d789aafd8f03f277c8e91daed95759b3
Dmytro Tsurko <22764205+dimkadt@users.noreply.github.com>
2022-05-02 09:21:48 +0300

    JE-61681 [Minio Cluster][MP] Update to the latest version (#5)
    * Update manifest.jps
    
    * Update manifest.jps
    
    * Update manifest.jps
    
    * Update manifest.jps
    
    * Update build-cluster.js
    
    * Update manifest.jps
    
    * Update build-cluster.js
    
    * Update manifest.jps
    
    * Update manifest.jps
    
    * JE-61681 [Minio Cluster][MP] Update to the latest version
    
    * JE-61681 [Minio Cluster][MP] Update to the latest version
    
    * JE-62560 [ Minio Cluster ] [ Updated version ] after scaling nodes count the new node is not added into servers list
    
    * JE-61681 [Minio Cluster][MP] Update to the latest version
    
    Co-authored-by: Dmitry Tsurko <dt@jelastic.com>

--- build-cluster.js
+++ build-cluster.js
@@ -1,5 +1,5 @@
 //@auth
-//@req(nodeGroup, volumePath)
+//@req(nodeGroup, volumePath, port)
 
 var envName = '${env.envName}', 
 nodes = jelastic.env.control.GetEnvInfo(envName, session).nodes,
@@ -15,7 +15,7 @@ if (servers.length == 1) servers[0] = volumePath;
   
 for (var i = 0; i < nodes.length; i++) { 
       if (nodes[i].nodeGroup != nodeGroup) continue; 
-      resp.push(jelastic.env.control.SetDockerRunCmd(envName, session, nodes[i].id, "server " + servers.join(' ') + " --address :$PORT"));
+      resp.push(jelastic.env.file.ReplaceInBody(envName, session, '/etc/default/minio', '^MINIO_VOLUMES=.*', "MINIO_VOLUMES=" + servers.join(' ') + " --address :" + port + " --console-address :" + consolePort, 1, null, null, false, nodes[i].id ));
 }
 
 return {
